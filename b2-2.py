665551480f0990450a40c5cdbe6f0d5b78b9368d
llhh8 <507877550@qq.com>
2023-05-02 16:18:17 +0800

    The First Initial commit

---
+++ b2-2.py
@@ -0,0 +1,109 @@
+# !usr/bin/env python
+# -*- coding:utf-8 _*-
+"""
+@Author:Sakurajima Mai
+@Blog(个人博客地址): blog.ixacg.com
+@Sakura:不积跬步无以至千里，不积小流无以成江海，程序人生的精彩需要坚持不懈地积累！
+"""
+import os
+from datetime import datetime
+from flask import Flask, request, render_template_string
+from b2sdk.v1 import InMemoryAccountInfo, B2Api
+import requests
+# # 设置B2存储相关信息
+B2_ACCOUNT_ID = "004771923a64e270000000006"
+B2_APPLICATION_KEY = "K004BT8mvEiMzf6iLrSbq1Aw9am6A6Y"
+B2_BUCKET_NAME = "Aimages"
+
+# 初始化Flask应用
+app = Flask(__name__)
+
+# B2认证
+info = InMemoryAccountInfo()
+b2_api = B2Api(info)
+b2_api.authorize_account("production", B2_ACCOUNT_ID, B2_APPLICATION_KEY)
+bucket = b2_api.get_bucket_by_name(B2_BUCKET_NAME)
+
+@app.route('/upload', methods=['POST'])
+def upload_files():
+    uploaded_files = request.files.getlist('files[]')
+    image_links = request.form.get('image_links', '').splitlines()
+    image_urls = []
+    html_image_links = []
+
+    for file in uploaded_files:
+        file_extension = os.path.splitext(file.filename)[1].lower()
+        if file_extension in ['.jpg', '.jpeg', '.png', '.gif']:
+            current_date = datetime.now().strftime('%Y/%m/%d/')
+            b2_filename = current_date + file.filename
+            bucket.upload_bytes(file.read(), b2_filename)
+            b2_file_url = f'https://{B2_BUCKET_NAME}.s3.us-west-002.backblazeb2.com/{b2_filename}'
+            image_urls.append(b2_file_url)
+            html_image_links.append(f'<img src="{b2_file_url}" alt="{file.filename}" />')
+
+    for image_url in image_links:
+        try:
+            response = requests.get(image_url)
+            response.raise_for_status()
+            file_extension = os.path.splitext(image_url)[1].lower()
+            if file_extension in ['.jpg', '.jpeg', '.png', '.gif']:
+                current_date = datetime.now().strftime('%Y/%m/%d/')
+                filename = os.path.basename(image_url)
+                b2_filename = current_date + filename
+                bucket.upload_bytes(response.content, b2_filename)
+                b2_file_url = f'https://{B2_BUCKET_NAME}.s3.us-west-002.backblazeb2.com/{b2_filename}'
+                image_urls.append(b2_file_url)
+                html_image_links.append(f'<img src="{b2_file_url}" alt="{filename}" />')
+        except Exception as e:
+            print(f"Error uploading {image_url}: {e}")
+
+    if image_urls:
+        return render_template_string('''
+            <h1>图片已上传</h1>
+            <ul>
+            {% for url in image_urls %}
+                <li><a href="{{ url }}" target="_blank">{{ url }}</a></li>
+            {% endfor %}
+            </ul>
+            <p>HTML 图片链接:</p>
+            <textarea id="htmlLinks" rows="10" cols="100" readonly>{% for link in html_image_links %}{{ link }}{% endfor %}</textarea>
+            <button onclick="copyLinks()">复制HTML链接</button>
+            <script>
+            function copyLinks() {
+              const textarea = document.getElementById('htmlLinks');
+              textarea.select();
+              document.execCommand('copy');
+            }
+            </script>
+            <p><a href="{{ url_for('index') }}">返回</a></p>
+            ''', image_urls=image_urls, html_image_links=html_image_links)
+    else:
+        return "上传失败，请重试"
+@app.route('/')
+def index():
+        return '''
+        <!doctype html>
+        <html>
+        <head>
+        <title>图片上传到B2存储</title>
+        </head>
+        <body>
+        <h1>上传图片到B2存储</h1>
+        <form action="/upload" method="post" enctype="multipart/form-data">
+        <label for="files">选择文件上传：</label>
+        <input type="file" name="files[]" multiple>
+        <br><br>
+        <label for="image_links">或粘贴图片链接（每行一个）：</label>
+        <br>
+        <textarea name="image_links" rows="10" cols="50"></textarea>
+        <br><br>
+        <input type="submit" value="上传">
+        </form>
+        </body>
+        </html>
+'''
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+# Aimages
+# 004771923a64e270000000006
+# K004BT8mvEiMzf6iLrSbq1Aw9am6A6Y
